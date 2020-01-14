6447b5076f16dd1e010d48dcc28ae9767a6579ef
Celia <celia@targetmedia.eu>
2020-01-14 15:04:19 +0100

    Minor changes

--- app/code/local/Targetpay/Bankwire/sql/bankwire_setup/mysql4-upgrade-0.1.1-0.1.2.php
+++
@@ -1,7 +0,0 @@
-<?php
-// here are the table creation for this module e.g.:
-$this->startSetup();
-
-$this->run("ALTER TABLE  `targetpay` ADD  `more` VARCHAR( 255 ) NOT NULL AFTER  `targetpay_txid`");
-
-$this->endSetup();
--- app/code/local/Targetpay/Paysafecard/sql/paysafecard_setup/mysql4-upgrade-0.1.2-0.1.3.php
+++
@@ -1,8 +0,0 @@
-<?php
-$this->startSetup();
-
-$this->run("
-	delete from " . Mage::getSingleton('core/resource')->getTableName('core_config_data') . " where path='payment/paysafecard/order_status'
-	");
-
-$this->endSetup();
